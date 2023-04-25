0c71cc45a891aa6e6ff10ec59b13ce56a682cca4
Nyllea <walid.outtaleb@ensta-paris.fr>
2023-04-25 08:15:57 +0200

    Ajout de commentaires et renommage

--- app/src/main/java/com/example/roboticarmhandapp/Hand.java
+++ app/src/main/java/com/example/roboticarmhandapp/Hand.java
@@ -19,8 +19,13 @@ import me.aflak.arduino.ArduinoListener;
 public class Hand extends Fragment implements ArduinoListener {
     private static final int ARDUINO_VENDOR_ID = 2341, ARDUINO_USB_PERMISSION_DELAY = 3;
 
-    TextView HandArduinoConnected;
+    // Zone de texte affichant l'état de l'arduino et les messages qu'elle renvoie
+    TextView HandArduinoTxtView;
+
+    // Les deux boutons de l'interface
     TouchableButton openHandButton,closeHandButton;
+
+    // L'arduino et un tableau de 1 byte pour lui envoyer des messages
     private Arduino arduino;
     byte[] sendSeeked = new byte[1];
 
@@ -28,6 +33,7 @@ public class Hand extends Fragment implements ArduinoListener {
         // Required empty public constructor
     }
 
+    // Code la valeur (comprise entre -128 et 127) sur un byte et l'envoie à l'arduino
     private void SendToArduino(int value) {
         // byte prend des valeurs entre 0 et 255: on effectue un décalage pour envoyer des valeurs entre -128 et 127
         sendSeeked[0] = (byte) (128 + value);
@@ -47,7 +53,7 @@ public class Hand extends Fragment implements ArduinoListener {
         // Declaration des views
         openHandButton = v.findViewById(R.id.openHandButton);
         closeHandButton = v.findViewById(R.id.closeHandButton);
-        HandArduinoConnected = v.findViewById(R.id.HandArduinoConnectedTxt);
+        HandArduinoTxtView = v.findViewById(R.id.HandArduinoTxt);
 
         // On envoie une commande à l'arduino tant que le bouton Fermer est maintenu
         closeHandButton.setOnTouchListener((View _v, MotionEvent event) -> {
@@ -97,21 +103,21 @@ public class Hand extends Fragment implements ArduinoListener {
     //Connecte la carte arduino si la permission est donnée
     @Override
     public void onArduinoAttached(UsbDevice device) {
-        HandArduinoConnected.setText(R.string.arduinoConnected);
+        HandArduinoTxtView.setText(R.string.arduinoConnected);
         arduino.open(device);
     }
 
     //Si la carte est déconnectée
     @Override
     public void onArduinoDetached() {
-        HandArduinoConnected.setText(R.string.arduinoDisconnected);
+        HandArduinoTxtView.setText(R.string.arduinoDisconnected);
     }
 
     // Quand on recoit un message de l'Arduino, on l'affiche
     @Override
     public void onArduinoMessage(byte[] bytes) {
         String msg = new String(bytes);
-        HandArduinoConnected.setText(msg);
+        HandArduinoTxtView.setText(msg);
     }
 
     @Override
@@ -123,7 +129,7 @@ public class Hand extends Fragment implements ArduinoListener {
     //Si la permission n'est pas accordée
     @Override
     public void onUsbPermissionDenied() {
-        HandArduinoConnected.setText(getString(R.string.arduinoPermissionDenied, ARDUINO_USB_PERMISSION_DELAY));
+        HandArduinoTxtView.setText(getString(R.string.arduinoPermissionDenied, ARDUINO_USB_PERMISSION_DELAY));
         new Handler().postDelayed(() -> arduino.reopen(), ARDUINO_USB_PERMISSION_DELAY);
     }
 
